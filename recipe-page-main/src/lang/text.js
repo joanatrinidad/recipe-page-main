32f4697a525c51879d2fc7e96b0e4bb52d712f63
iameffieeee <joanatrinidad12@yahoo.com>
2026-03-28 20:46:39 +0800

    practice - recipe page

---
+++ recipe-page-main/src/lang/text.js
@@ -0,0 +1,43 @@
+export const recipe = {
+  header: {
+    title: "Simple Omelette Recipe",
+    subtitle:
+      "An easy and quick dish, perfect for any meal. This classic omelette combines beaten eggs cooked to perfection, optionally filled with your choice of cheese, vegetables, or meats.",
+  },
+  preparation: {
+    title: "Preparation time",
+    total: "Total: Approximately 10 minutes",
+    prep: "Preparation: 5 minutes",
+    cook: "Cooking: 5 minutes",
+  },
+  ingredients: {
+    title: "Ingredients",
+    ing1: "2-3 large eggs",
+    ing2: "Salt, to taste",
+    ing3: "Pepper, to taste",
+    ing4: "1 tablespoon of butter or oil",
+    ing5: "Optional fillings: cheese, diced vegetables, cooked meats, herbs",
+  },
+  instructions: {
+    title: "Instructions",
+    ins1: "Beat the eggs: In a bowl, beat the eggs with a pinch of salt and pepper until they are well mixed. You can add a tablespoon of water or milk for a fluffier texture.",
+    ins2: "Heat the pan: Place a non-stick frying pan over medium heat and add butter or oil.",
+    ins3: "Cook the omelette: Once the butter is melted and bubbling, pour in the eggs. Tilt the pan to ensure the eggs evenly coat the surface.",
+    ins4: "Add fillings (optional): When the eggs begin to set at the edges but are still slightly runny in the middle, sprinkle your chosen fillings over one half of the omelette.",
+    ins5: "Fold and serve: As the omelette continues to cook, carefully lift one edge and fold it over the fillings. Let it cook for another minute, then slide it onto a plate.",
+    ins6: "Enjoy: Serve hot, with additional salt and pepper if needed.",
+  },
+  nutrition: {
+    title: "Nutrition",
+    subtitle:
+      "The table below shows nutritional values per serving without the additional fillings.",
+    cal: "Calories",
+    totalCal: "277kcal.",
+    carbs: "Carbs",
+    totalCarbs: "0g",
+    protein: "Protein",
+    totalProtein: "20g",
+    fat: "Fat",
+    totalFat: "22g",
+  },
+};
